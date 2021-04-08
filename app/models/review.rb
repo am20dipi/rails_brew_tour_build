5614761f72eb22c76b07e1cc7055f804946f5b59
Adriana <am20dipi@alum.siena.edu>
2021-04-08 16:36:30 -0400

    generated controllers

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,7 @@ class Review < ApplicationRecord
 
     validates :title, presence: true
     validates :content, presence: true 
-    validates :rating, presence: true, numericality: true 
-    
+    validates :rating, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 6 } 
+    validates :beer, uniqueness: { scope: :user, message: "You already reviewed this one!"}
 
 end
