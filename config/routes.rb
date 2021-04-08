b43cd96b0c0924c4d88c63ce92aa9ac82d00ac2b
Adriana <am20dipi@alum.siena.edu>
2021-04-08 16:38:20 -0400

    Added controller actions to BeersController, added resources to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :beers
+  resources :users
+  resources :breweries
+  resources :reviews 
 end
