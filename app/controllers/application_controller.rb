f68c22494fefb9a964663322169a9134003febf9
Adriana <am20dipi@alum.siena.edu>
2021-04-08 17:05:06 -0400

    added helper functionality macro for views, added nav bar in layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+    helper_method :logged_in?
+    # gives us access to these methods in the views
+
     def logged_in?
         !!session[:user_id]
     end
