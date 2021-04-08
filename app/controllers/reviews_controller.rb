8280067d5d41f98de34bb9846144a07a64f8ad60
Adriana <am20dipi@alum.siena.edu>
2021-04-08 16:44:02 -0400

    Added controller actions to ReviewsController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,45 @@
 class ReviewsController < ApplicationController
+    
+    def index
+        @reviews = Review.all
+    end
+
+    def new
+        @review = Review.new
+    end
+
+    def create 
+        @review = Review.create(review_params)
+        if @review.valid?
+            redirect_to @beer 
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        @review.update 
+        if @review.save
+            redirect_to @beer
+        else
+            render :edit
+        end
+    end
+
+    def destroy 
+        @review.destroy
+        redirect_to @beer
+    end
+
+    private
+    def find_review
+        @review = Review.find_by(id: params[:id])
+    end
+
+    def review_params
+        params.require(:review).permit(:title, :content, :rating)
+    end
 end
