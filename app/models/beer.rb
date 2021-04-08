dc8248ea27ac7ccd49e5d6496671c7d62b6d3685
Adriana <am20dipi@alum.siena.edu>
2021-04-08 16:31:09 -0400

    Add associations to models

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,7 @@
 class Beer < ApplicationRecord
-    
+    has_many :reviews
+    has_many :users, through: :reviews
+
+    belongs_to :brewery 
+    belongs_to :user 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :reviews
+    has_many :beers, through: :reviews 
+    
 end
