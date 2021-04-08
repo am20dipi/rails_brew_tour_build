d966bb9f34064979e3d7131412fb58d5376ae3c0
Adriana <am20dipi@alum.siena.edu>
2021-04-08 16:39:04 -0400

    Added controller actions to UsersControlelr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            # log them in, since they are now signed up
+            session[:user_id] = @user.id
+
+            #redirect to the show page, "@user" is an abstraction of the user_path
+            redirect_to @user 
+        else
+            render :new
+        end
+
+    end
+
+    def show
+        redirect_if_not_logged_in
+        @user = User.find_by_id(params[:id])
+        # what is the difference between "find_by_id" & "find"?
+        # "find" will portray an error if a user is not found; "find_by_id" will return NIL"
+        redirect_to '/' if !@user
+        # protection: redirect to home page is user is not found/does not exist
+    end
+
+
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
 end
